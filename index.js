ec675659350fd308902c5b0433ea3ffb8f17ac01
Markus Schmidt <m.schmidt@acemail.de>
2016-03-28 22:11:45 +0200

    Removed loose mode code

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ module.exports = {
 	presets: [require("babel-preset-es2015")],
 	plugins: [
 		require("babel-plugin-transform-class-properties"),
-		require('babel-plugin-transform-mahalo'),
 		require('babel-plugin-transform-flow-strip-types'),
-		require('babel-plugin-transform-runtime'),
-		[require('babel-plugin-transform-es2015-classes'), {loose: true}]
+		require('babel-plugin-transform-mahalo'),
+		require('babel-plugin-transform-runtime')
 	]
 };
