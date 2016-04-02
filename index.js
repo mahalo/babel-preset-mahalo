25be59f1f43d6d5a5778301a2f662e5af2cd3c2e
Markus Schmidt <m.schmidt@acemail.de>
2016-04-02 13:54:53 +0200

    Changed order to make split points work

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
 	presets: [require("babel-preset-es2015")],
 	plugins: [
+		require('babel-plugin-transform-mahalo'),
 		require("babel-plugin-transform-class-properties"),
 		require('babel-plugin-transform-flow-strip-types'),
-		require('babel-plugin-transform-mahalo'),
 		require('babel-plugin-transform-runtime')
 	]
 };
