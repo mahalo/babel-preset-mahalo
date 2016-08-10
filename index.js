27e859ecadb2089fb2f11a68c13ffad9c2d3bef2
Markus Schmidt <m.schmidt@makuko.de>
2016-08-10 13:37:38 +0200

    Moved polyfills to separate packages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
     presets: [require("babel-preset-es2015")],
     plugins: [
+        require('babel-runtime-es2015-instance-methods'),
+        require('babel-plugin-transform-es2015-classes-ielt10'),
         require('babel-plugin-transform-mahalo'),
         require("babel-plugin-transform-class-properties"),
         require('babel-plugin-transform-flow-strip-types'),
